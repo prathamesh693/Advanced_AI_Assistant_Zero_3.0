99d29b645a6a715e862ef2e97bc545b36d1c2e49
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-08-16 17:23:10 +0530

    Check PC online status function

---
+++ FUNCTION/CHECK_ONLINE_OFFLINE_STATUS/check_online_offline_status.py
@@ -0,0 +1,19 @@
+import requests
+
+def is_online(url="https://www.google.com",timeout=5):
+    try:
+        # Try to make a GET request to the specified URL
+        response = requests.get(url, timeout=timeout)
+        #Check if the respose status code is in the success range (200-299)
+        return response.status_code >= 200 and response.status_code < 300
+    except requests.ConnectionError:
+        return False
+
+# Example usage
+def internet_status():
+    if is_online():
+        x="online"
+        return x
+    else:
+        x="offline"
+        return x
