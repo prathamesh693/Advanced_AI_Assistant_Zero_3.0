c9b150002c982b3d5576ed2f4e6b1294092582b9
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-08-20 18:13:14 +0530

    Google Scroll functions

---
+++ AUTOMATION/ZERO_AUTOMATION_GOOGLE/SCROLL_ON_GOOGLE/scroll_automation.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+
+# ====================
+# ⬆ Scrolling (via shortcuts)
+# ====================
+def scroll_down():
+    # Scroll down by pressing the Down arrow key
+    pyautogui.press('down')
+
+def scroll_up():
+    # Scroll up by pressing the Up arrow key
+    pyautogui.press('up')
+
+def scroll_to_top():
+    pyautogui.hotkey('home')
+
+def scroll_to_bottom():
+    pyautogui.hotkey('end')
