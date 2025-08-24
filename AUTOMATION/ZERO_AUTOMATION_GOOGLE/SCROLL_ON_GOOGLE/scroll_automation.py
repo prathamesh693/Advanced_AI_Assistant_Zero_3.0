1abb092593f7d68193c040371f87c3d8101f6127
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-08-24 14:07:35 +0530

    multi scroll call

--- AUTOMATION/ZERO_AUTOMATION_GOOGLE/SCROLL_ON_GOOGLE/scroll_automation.py
+++ AUTOMATION/ZERO_AUTOMATION_GOOGLE/SCROLL_ON_GOOGLE/scroll_automation.py
@@ -8,9 +8,15 @@ import time
 def scroll_down():
     # Scroll down by pressing the Down arrow key
     pyautogui.press('down')
+    pyautogui.press('down')
+    pyautogui.press('down')
+    pyautogui.press('down')
 
 def scroll_up():
     # Scroll up by pressing the Up arrow key
+    pyautogui.press('up')
+    pyautogui.press('up')
+    pyautogui.press('up')
     pyautogui.press('up')
 
 def scroll_to_top():
