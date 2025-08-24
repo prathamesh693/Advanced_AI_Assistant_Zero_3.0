9d0e21e30b6cb45d1ae0da96391a39a858024fb6
Jadhav Prathamesh Suresh <prathameshj693@gmail.com>
2025-08-24 14:11:10 +0530

    Jokes function

--- BRAIN/ACTIVITY/JOKE/jokes.py
+++ BRAIN/ACTIVITY/JOKE/jokes.py
@@ -1,5 +1,8 @@
+import random
 import requests
+from FUNCTION.ZERO_LISTEN.listen import listen
 from FUNCTION.ZERO_SPEAK.speak import speak
+import time
 
 
 def get_random_joke():
@@ -9,5 +12,19 @@ def get_random_joke():
     res=requests.get("https://icanhazdadjoke.com/", headers=headers).json()
     return res["joke"]
 
+def jokes():
+    while True:
+        x=[600,550,580,400,3000,800,700,8200,8000,50,568]
+        x=random.choice(x)
+        time.sleep(x)
+        speak("I have some joke for you, sir")
+        text=listen().lower()
+        if "yes tell me" in text or "yes" in text:
+            advice =get_random_joke()
+            speak(advice)
+            pass
+        else:
+            speak("no problem sir, I just want to include some entertainment in your day")
+            pass
 #x=get_random_joke()
 #print(x)
